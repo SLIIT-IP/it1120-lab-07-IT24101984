65270aabd75323ae38e34c54f4d29f3c1a9dd89b
Vithanage T.A <it24101984@my.sliit.lk>
2024-10-01 21:51:37 +0530

    Add IT24101984Lab7Q2C.java to Q2

---
+++ Q2/IT24101984Lab7Q2C.java
@@ -0,0 +1,24 @@
+public class IT24101984Lab7Q2C {
+    public static void main(String[] args) {
+
+        int i,j;
+        int k =5;
+
+        //Outer loop
+        for(i = 5; i>=1; i--) {
+
+            //Inner Loop
+            for(j = 1; j <= k; j++) {
+                System.out.print(i);
+                
+            }
+            k--;
+            System.out.print("\n");
+        
+        }
+
+
+
+    }
+}
+ 
