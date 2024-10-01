b6802a059b56bf44a8a840df5ae356a8be36816f
Vithanage T.A <it24101984@my.sliit.lk>
2024-10-01 21:50:18 +0530

    Add IT24101984Lab7Q2A.java to Q2

---
+++ Q2/IT24101984Lab7Q2A.java
@@ -0,0 +1,17 @@
+public class IT24101984Lab7Q2A {
+    public static void main(String[] args) {
+
+        char star = '$';
+
+        
+
+        for (int count = 1 ; count<=4; count++) {
+            System.out.println(star +"  "+ star+"  "+ star+"  "+star + "  "+ star);
+        }
+
+
+
+
+
+    }
+}
